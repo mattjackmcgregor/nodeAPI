dcc17530c1c03567058b9ee449e4171c26f242ad
user <mattjackmcgregor@gmail.com>
2020-05-12 21:54:03 +1200

    handling duplicate field error

--- middleware/errorHandling.js
+++ middleware/errorHandling.js
@@ -5,15 +5,23 @@ const errorHandler = (err, req, res, next) => {
 
   error.message = err.message
   //logging to console for dev
-  console.log(err.stack.red)
+  console.log(err)
   console.log(err.name)
 
   //Mongoose bad ObjectId
   if(err.name === 'CastError'){
     const message = `Resourse with id ${err.value} doesnt exits`
     error = new ErrorResponse(message, 404)
-    
   }
+
+  //Mongoose Duplicate value
+  if(err.code === 11000) {
+    const message = "duplicate field value entered"
+    error = new ErrorResponse(message, 400)
+  }
+
+  //
+
   res.status(error.statusCode || 500).json({
     success: false,
     error: error.message || 'Server Error'
