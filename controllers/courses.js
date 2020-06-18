2f39a49c8ae20704a64232450e5aab078a20d910
user <mattjackmcgregor@gmail.com>
2020-06-18 12:26:19 +1200

    created controller method and route for updating a course

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,4 +1,5 @@
 const Course = require('../models/Course')
+const Bootcamp= require('../models/Bootcamp')
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/async')
 
@@ -25,5 +26,66 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
     sucess: true,
     count: courses.length,
     data: courses
+  })
+})
+
+// @desc      get single course
+// @route     api/v1/courses/:id
+// @access    Public
+exports.getCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id).populate({
+    path: 'bootcmap',
+    select: 'name description'
+  })
+
+  if(!course) {
+     return next(new ErrorResponse('course with this id doesnt exits', 404))
+  }
+
+  res.status(200).json({
+    sucess: true,
+    data: course
+  })
+})
+
+// @desc      create course
+// @route     api/v1/bootcamps/:bootcampId/courses
+// @access    Private
+exports.createCourse = asyncHandler(async (req, res, next) => {
+  //getting bootcamp that the course is for
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId)
+  //setting the bootcamp id in the request body
+  req.body.bootcamp = req.params.bootcampId
+
+  if(!bootcamp) {
+     return next(new ErrorResponse('bootcamp with this id doesnt exits', 404))
+  }
+
+  const course = await Course.create(req.body)
+
+  res.status(201).json({
+    sucess: true,
+    msg: 'course created',
+    data: course
+  })
+})
+
+// @desc      update course
+// @route     api/v1//courses/:id
+// @access    Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  })
+
+  if (!course) {
+    return next(new ErrorResponse('course with this id doesnt exits', 404))
+  }
+
+  res.status(201).json({
+    sucess: true,
+    msg: 'course created',
+    data: course
   })
 })
--- routes/courses.js
+++ routes/courses.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const {
-  getCourses
+  getCourses,
+  getCourse,
+  createCourse,
+  updateCourse
 } = require('../controllers/courses')
 
 const router = express.Router({mergeParams: true})
 
 router.route('/')
-.get(getCourses)
+  .get(getCourses)
+  .post(createCourse) // doesnt need full /bootcamp/:bootcampId etc because of the redirect in bootcamps router
+
+router.route('/:id')
+  .get(getCourse)
+  .put(updateCourse)
+
 
 module.exports = router
