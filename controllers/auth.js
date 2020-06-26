5260d109268663cc6a0148d72a5acfb1f0eea97a
user <mattjackmcgregor@gmail.com>
2020-06-26 17:32:42 +1200

    creted login route controller and password match method

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,18 +4,20 @@ const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/async')
 
 // @desc      Register user
-// @route     GET api/v1/auth/register
+// @route     POST api/v1/auth/register
 // @access    Public
 exports.registerUser = asyncHandler (async (req, res, next) => {
   //grabing user data from the body
-  const {name, email, password} = req.body
+  const {name, email, password, role} = req.body
 
+  //creating user obj for database
   const user = await User.create({
     name,
     email,
-    password
+    password,
+    role
   })
-
+  //getting signed JWT token 
   const token = user.getSignedJwtToken()
 
   res.status(201).json({
@@ -23,5 +25,44 @@ exports.registerUser = asyncHandler (async (req, res, next) => {
     msg: 'created user',
     data: token
   })
+})
 
+// @desc      Login user
+// @route     POST api/v1/auth/login
+// @access    Public
+exports.loginUser = asyncHandler(async (req, res, next) => {
+  //grabing user data from the body
+  const {
+    email,
+    password,
+  } = req.body
+
+  //validate email and password
+  if (!email || !password) {
+    return next(new ErrorResponse('please provide valid email and password', 400))
+  }
+
+  //getting users email and typed password
+  const user = await User.findOne({email}).select('+password')
+
+  //validate user
+  if(!user) {
+    return next(new ErrorResponse('invalid credentials', 401))
+  }
+
+  //check password
+  const isMatch = await user.matchPassword(password)
+
+  if(!isMatch){
+    return next(new ErrorResponse('invalid credentials', 401))
+  }
+
+  //getting signed JWT token 
+  const token = user.getSignedJwtToken()
+
+  res.status(201).json({
+    sucess: true,
+    msg: 'logged in',
+    data: token
+  })
 })
--- models/User.js
+++ models/User.js
@@ -48,4 +48,9 @@ UserSchema.methods.getSignedJwtToken = function() {
   })
 }
 
+//matching password
+UserSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password)
+}
+
 module.exports = mongoose.model('User', UserSchema)
