348056e9b7da63df58c669b3da391f7bb05e8183
user <mattjackmcgregor@gmail.com>
2020-06-26 18:45:38 +1200

    set httpOnly to true

--- controllers/auth.js
+++ controllers/auth.js
@@ -61,8 +61,12 @@ const sendTokenResponse = (user, statusCode, res) => {
    const token = user.getSignedJwtToken()
     //setting cookie expiry 30 days from now time
    const options = {
-     expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES *24 *60 *60 *1000)
+     expires: new Date(
+       Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+     ),
+     httpOnly: true
    }
+   
 
    res
     .status(statusCode)
